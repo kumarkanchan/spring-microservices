3a6a61c24a43818a04056577f914da27ac4f4f64
vikas <vikaskanvika@gmail.com>
2020-05-30 02:57:56 +0530

    first commit

---
+++ currency-conversion-service/src/test/java/com/boot/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.microservices.currencyconversionservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
