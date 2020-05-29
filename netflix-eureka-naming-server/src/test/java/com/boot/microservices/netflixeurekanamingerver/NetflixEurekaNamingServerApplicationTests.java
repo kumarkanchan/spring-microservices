094a73ed456779a0f89f153b73b6171a0e841556
vikas <vikaskanvika@gmail.com>
2020-05-30 03:17:34 +0530

    first commit

---
+++ netflix-eureka-naming-server/src/test/java/com/boot/microservices/netflixeurekanamingerver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.microservices.netflixeurekanamingerver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
