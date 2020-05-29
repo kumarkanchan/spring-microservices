7d180562f94d8d26444546a5234daffdf4f0954a
vikas <vikaskanvika@gmail.com>
2020-05-30 03:08:42 +0530

    first commit

---
+++ api-gateway/src/main/java/com/microservice/demo/apigateway/beans/Employee.java
@@ -0,0 +1,35 @@
+package com.microservice.demo.apigateway.beans;
+
+
+	public class Employee {
+
+		private String name;
+		private int id;
+
+		public Employee(int id, String name) {
+			this.name = name;
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		@Override
+		public String toString() {
+			return "Employee [name=" + name + ", id=" + id + "]";
+		}
+	
+}
